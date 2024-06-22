3d72c566b5674e139d30cfcf11ba6740dcdad8e4
Naru <tristan.etude@gmail.com>
2024-06-22 19:56:24 +0200

    Ajout de pages et de modules

---
+++ FrontEnd/src/components/EditName/EditName.jsx
@@ -0,0 +1,27 @@
+import Button from "../Button/Button";
+
+const EditName = () => {
+  return (
+    <section className="sign-in-content">
+      <i className="fa fa-user-circle sign-in-icon"></i>
+      <h1>Edit User info</h1>
+      <form>
+        <div className="input-wrapper">
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" />
+        </div>
+        <div className="input-wrapper">
+          <label htmlFor="firstname">First Name</label>
+          <input type="text" id="firstname" />
+        </div>
+        <div className="input-wrapper">
+          <label htmlFor="lastname">Last Name</label>
+          <input type="text" id="lastname" />
+        </div>
+        <Button btnText={"Save"}/>
+        <Button btnText={"Cancel"}/>
+      </form>
+    </section>
+  );
+};
+export default EditName;
